6e30b982a98dcda9303a7171ff058a0ec03c68d1
Olivier Boulant <olivier.boulant@proton.me>
2025-07-09 17:08:40 +0200

    flower-field

--- exercises/practice/flower-field/src/lib.rs
+++ exercises/practice/flower-field/src/lib.rs
@@ -1,5 +1,37 @@
+fn get_local_char(garden: &[&str], i: usize, j: usize, vicinity: usize) -> char {
+    let nb_rows = garden.len();
+    let nb_cols = garden.get(0).unwrap().len();
+
+    match (i.checked_sub(vicinity).unwrap_or(0) ..= i.checked_add(vicinity).unwrap().min(nb_rows-1))
+    .flat_map(|cline| 
+        (j.checked_sub(vicinity).unwrap_or(0)
+        ..= 
+        j.checked_add(vicinity).unwrap().min(nb_cols-1))
+        .map(move |ccol| {
+            println!("cline {cline}, ccol {ccol}");
+            (cline, ccol)
+        }))
+    .filter(|&(cline, ccol)| *garden.get(cline).unwrap().as_bytes().get(ccol).unwrap() == b'*' ) // keep only cells with a flower
+    .count() {
+        0 => ' ', // If 0 flower, do not display '0', rather leave a space
+        count => char::from_digit(count as u32, 10).unwrap()
+    }
+}
+
 pub fn annotate(garden: &[&str]) -> Vec<String> {
-    todo!(
-        "\nAnnotate each square of the given garden with the number of flowers that surround said square (blank if there are no surrounding flowers):\n{garden:#?}\n"
-    );
+    garden
+    .iter().enumerate()
+    .map(|(i, line)| {
+        line
+        .bytes()
+        .enumerate()
+        .map(|(j, c)| {
+            match c {
+                b'*' => '*', // If flower, leave it here
+                _ => get_local_char(garden, i, j, 1) // Otherwise, compute surroundings flowers
+            }
+        })
+        .collect()
+    })
+    .collect()
 }
