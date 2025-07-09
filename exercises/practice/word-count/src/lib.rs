33a2d7184014420b07a8c3f7d98aa0833c3a492d
Olivier Boulant <olivier.boulant@proton.me>
2025-07-09 18:26:26 +0200

    lint

--- exercises/practice/word-count/src/lib.rs
+++ exercises/practice/word-count/src/lib.rs
@@ -2,16 +2,19 @@ use std::collections::HashMap;
 
 /// Count occurrences of words.
 pub fn word_count(words: &str) -> HashMap<String, u32> {
-    let mut res:HashMap<String, u32> = HashMap::new();
+    let mut res: HashMap<String, u32> = HashMap::new();
 
     words
-    .split(|c: char| c.is_whitespace() || (c.is_ascii_punctuation() && c != '\''))
-    .for_each(|w| {
-        let key =  w.trim_matches(|c:char| !c.is_alphanumeric()).to_string().to_lowercase();
-        if !key.is_empty() { 
-            res.entry(key).and_modify(|e| {*e += 1}).or_insert(1);
-        }
-    });
+        .split(|c: char| c.is_whitespace() || (c.is_ascii_punctuation() && c != '\''))
+        .for_each(|w| {
+            let key = w
+                .trim_matches(|c: char| !c.is_alphanumeric())
+                .to_string()
+                .to_lowercase();
+            if !key.is_empty() {
+                res.entry(key).and_modify(|e| *e += 1).or_insert(1);
+            }
+        });
 
     println!("{res:?}");
 
