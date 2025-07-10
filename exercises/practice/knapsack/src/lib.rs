30271b4fd46a4e1ef6f6639a70071b1079ba7966
Olivier Boulant <olivier.boulant@proton.me>
2025-07-10 14:37:15 +0200

    knapsack

--- exercises/practice/knapsack/src/lib.rs
+++ exercises/practice/knapsack/src/lib.rs
@@ -1,3 +1,5 @@
+use itertools::Itertools;
+
 #[derive(Debug)]
 pub struct Item {
     pub weight: u32,
@@ -5,5 +7,26 @@ pub struct Item {
 }
 
 pub fn maximum_value(max_weight: u32, items: &[Item]) -> u32 {
-    todo!("calculate the maximum value achievable with the given {items:?} and {max_weight}");
+    match (0..=items.len())
+        .flat_map(|l| items.iter().combinations(l))
+        .filter(|combination| {
+            // Sum of weight should be less than max_weight
+            let mut total_weight: u32 = 0;
+            for &item in combination {
+                total_weight += item.weight;
+            }
+
+            total_weight <= max_weight && total_weight > 0
+        })
+        .map(|combination| {
+            // For each combination, compute the total value
+            combination.iter().map(|&item| item.value).sum::<u32>()
+        })
+        .collect::<Vec<u32>>()
+        .iter()
+        .max()
+    {
+        Some(max) => *max,
+        None => 0,
+    }
 }
