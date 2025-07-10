6f4d69296647631fba5020e0d287b52f51861845
Olivier Boulant <olivier.boulant@proton.me>
2025-07-10 11:28:43 +0200

    secret-handshake

--- exercises/practice/secret-handshake/src/lib.rs
+++ exercises/practice/secret-handshake/src/lib.rs
@@ -1,3 +1,15 @@
 pub fn actions(n: u8) -> Vec<&'static str> {
-    todo!("What is the secret handshake for {n}?")
+    let actions = ["wink", "double blink", "close your eyes", "jump"];
+    let mut res: Vec<&'static str> = Vec::new();
+
+    for (i, action) in actions.iter().enumerate() {
+        if n & (0b000_0001 << i) != 0 {
+            res.push(action);
+        }
+    }
+
+    if n & 0b001_0000 != 0 {
+        res.reverse()
+    }
+    res
 }
