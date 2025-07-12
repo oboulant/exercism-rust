1821873f3e4777a88f2f6d5b93dc3bf673c3f7e3
Olivier Boulant <olivier.boulant@proton.me>
2025-07-12 13:40:12 +0200

    matrix

--- exercises/practice/matrix/src/lib.rs
+++ exercises/practice/matrix/src/lib.rs
@@ -1,17 +1,44 @@
 pub struct Matrix {
-    // Implement your Matrix struct
+    elements: Vec<u32>,
+    nrow: usize,
+    ncol: usize,
 }
 
 impl Matrix {
     pub fn new(input: &str) -> Self {
-        todo!("Create new method to store the {input}")
+        let lines = input.split("\n");
+        let nrow = lines.clone().count();
+        let elements: Vec<u32> = lines
+            .flat_map(|line| line.split(' ').map(|el| el.parse::<u32>().unwrap()))
+            .collect();
+        let ncol = elements.clone().len() / nrow;
+
+        println!("nrow {nrow}, elements {elements:?}, ncol {ncol}");
+        Self {
+            nrow,
+            elements,
+            ncol,
+        }
     }
 
     pub fn row(&self, row_no: usize) -> Option<Vec<u32>> {
-        todo!("Return the row at {row_no} (1-indexed) or None if the number is invalid")
+        if row_no > self.nrow {
+            return None;
+        }
+
+        self.elements
+            .get(((row_no - 1) * self.ncol)..(row_no * self.ncol))
+            .map(|s| s.to_vec())
     }
 
     pub fn column(&self, col_no: usize) -> Option<Vec<u32>> {
-        todo!("Return the column at {col_no} (1-indexed) or None if the number is invalid")
+        if col_no > self.ncol {
+            return None;
+        }
+
+        ((col_no - 1)..(self.nrow * self.ncol))
+            .step_by(self.ncol)
+            .map(|i| self.elements.get(i).copied())
+            .collect()
     }
 }
