400cf8a979ba0e352fd0e8b1a2450bed9d4f0efd
Olivier Boulant <olivier.boulant@proton.me>
2025-07-13 00:19:49 +0200

    fizzy

--- exercises/practice/fizzy/src/lib.rs
+++ exercises/practice/fizzy/src/lib.rs
@@ -1,47 +1,82 @@
-// the PhantomData instances in this file are just to stop compiler complaints
-// about missing generics; feel free to remove them
-
-/// A Matcher is a single rule of fizzbuzz: given a function on T, should
-/// a word be substituted in? If yes, which word?
-pub struct Matcher<T>(std::marker::PhantomData<T>);
+/**
+ * An alternative would have been
+ * struct Matcher<T, F>
+ * where
+ *    F: Fn(T),
+ * {
+ *    callback: F,
+ * }
+ * but each F creates a different concrete type of Matcher, so it can’t be used generically at runtime or stored in containers.
+ **/
+/**
+ * `dyn` is needed because without it (`Box<Fn(i32)>`, which does not work and is ambiguous), Rust cannot infer whether you want:
+ *  - a generic type implementing the trait (static dispatch),
+ *  - or a trait object (dynamic dispatch).
+**/
+pub struct Matcher<T> {
+    is_match: Box<dyn Fn(T) -> bool>, // Box is needed because Fn have unknown size.
+    substitute: String,
+}
 
 impl<T> Matcher<T> {
-    pub fn new<F, S>(_matcher: F, _subs: S) -> Matcher<T> {
-        todo!()
+    pub fn new<F: Fn(T) -> bool + 'static, S: ToString>(_matcher: F, _subs: S) -> Matcher<T> {
+        // 'static says that F me be a function or closure owns everything (i.e. doesn't borrow local variables) and is validd for the whole duration of the program
+        Self {
+            is_match: Box::new(_matcher),
+            substitute: _subs.to_string(),
+        }
     }
 }
 
-/// A Fizzy is a set of matchers, which may be applied to an iterator.
-///
-/// Strictly speaking, it's usually more idiomatic to use `iter.map()` than to
-/// consume an iterator with an `apply` method. Given a Fizzy instance, it's
-/// pretty straightforward to construct a closure which applies it to all
-/// elements of the iterator. However, we're using the `apply` pattern
-/// here because it's a simpler interface for students to implement.
-///
-/// Also, it's a good excuse to try out using impl trait.
-pub struct Fizzy<T>(std::marker::PhantomData<T>);
+pub struct Fizzy<T> {
+    matchers: Vec<Matcher<T>>,
+}
 
-impl<T> Fizzy<T> {
+impl<T: ToString + Copy> Fizzy<T> {
     pub fn new() -> Self {
-        todo!()
+        Self {
+            matchers: Vec::new(),
+        }
     }
 
-    // feel free to change the signature to `mut self` if you like
     #[must_use]
-    pub fn add_matcher(self, _matcher: Matcher<T>) -> Self {
-        todo!()
+    pub fn add_matcher(mut self, _matcher: Matcher<T>) -> Self {
+        self.matchers.push(_matcher);
+        self
     }
 
-    /// map this fizzy onto every element of an iterator, returning a new iterator
-    pub fn apply<I>(self, _iter: I) -> impl Iterator<Item = String> {
-        // todo!() doesn't actually work, here; () is not an Iterator
-        // that said, this is probably not the actual implementation you desire
-        Vec::new().into_iter()
+    pub fn apply<I: Iterator<Item = T>>(self, _iter: I) -> impl Iterator<Item = String> {
+        _iter // first outter iteration on the input sequence
+            .map(move |el| {
+                // then apply all matchers
+                let res: String = self
+                    .matchers
+                    .iter()
+                    .filter(|&matcher| (matcher.is_match)(el)) // parenthesis needed because `is_match` is a field, not a method
+                    .map(|matcher| matcher.substitute.clone())
+                    .collect();
+
+                if res.is_empty() {
+                    // no match
+                    el.to_string()
+                } else {
+                    res
+                }
+            })
     }
 }
 
 /// convenience function: return a Fizzy which applies the standard fizz-buzz rules
-pub fn fizz_buzz<T>() -> Fizzy<T> {
-    todo!()
+pub fn fizz_buzz<
+    T: std::ops::Rem<Output = T> + std::cmp::PartialEq + From<u8> + Copy + std::fmt::Display,
+>() -> Fizzy<T> {
+    Fizzy::<T>::new()
+        .add_matcher(Matcher::new(
+            |n: T| n % T::from(3) == T::from(0),
+            "fizz".to_string(),
+        ))
+        .add_matcher(Matcher::new(
+            |n: T| n % T::from(5) == T::from(0),
+            "buzz".to_string(),
+        ))
 }
